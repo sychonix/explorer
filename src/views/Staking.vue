6d33358a61496c7c152e5776e36dd6fa2dbc5b5f
genznodes <94878333+Genz22@users.noreply.github.com>
2022-11-22 15:09:41 +0700

    update

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Helping Ping.pub By Staking ❤️"
+      title="Stake with genznodes ❤️"
       class="overflow-auto"
     >
       <b-table
