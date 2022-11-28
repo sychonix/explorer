a8e18944ca0c1e84c312a68dfd9a3a39633443af
genznodes <94878333+Genz22@users.noreply.github.com>
2022-11-28 12:58:34 +0700

    update

--- themeConfig.js
+++ themeConfig.js
@@ -11,7 +11,7 @@ export const $themeConfig = {
   app: {
     appName: 'Explorer', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/public/GN.png', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/public/logo.png', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
