a0f324a6e7cb461abbf4ac153acab39b98fe54ac
sychonix <110755590+sychonix@users.noreply.github.com>
2023-06-26 01:30:21 +0700

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Stake with genznodes ❤️"
+      title="Stake with Sychonix"
       class="overflow-auto"
     >
       <b-table
@@ -341,7 +341,7 @@ export default {
   },
   computed: {
     pingVals() {
-      return this.list.filter(x => x.description.identity === '86D0A9427E51D239')
+      return this.list.filter(x => x.description.identity === '6366E6C36DFCFCA7')
     },
     list() {
       const tab = this.selectedStatus === 'active' ? this.validators : this.inactiveValidators
