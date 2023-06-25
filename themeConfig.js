6d32d550dbfc20e22776b5f82d686522279b90e6
sychonix <110755590+sychonix@users.noreply.github.com>
2023-06-26 02:18:11 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,9 +9,9 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'Explorer', // Will update name in navigation menu (Branding)
+    appName: 'EXPLORER', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/logo2.png', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/logo.svg', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
