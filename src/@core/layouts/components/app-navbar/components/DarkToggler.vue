b96fdc5a03855bf513f51b4ceaa93fa8b8011343
sychonix <110755590+sychonix@users.noreply.github.com>
2023-06-27 00:58:13 +0700

    Update DarkToggler.vue

--- src/@core/layouts/components/app-navbar/components/DarkToggler.vue
+++ src/@core/layouts/components/app-navbar/components/DarkToggler.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-nav-item @click="toggleDarkMode" v-if="!isDark">
+  <b-nav-item @click="toggleDarkMode" :disabled="isDark">
     <feather-icon size="21" :icon="`${isDark ? 'Sun' : 'Moon'}Icon`" />
   </b-nav-item>
 </template>
@@ -19,7 +19,9 @@ export default {
     const isDark = computed(() => skin.value === 'dark')
 
     const toggleDarkMode = () => {
-      useAppConfig().setSkin('dark')
+      if (!isDark.value) {
+        useAppConfig().setSkin('dark')
+      }
     }
 
     return { isDark, toggleDarkMode }
